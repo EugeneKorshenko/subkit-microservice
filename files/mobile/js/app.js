1ea3b46e67ee230125e707844b27a3c697d7c207
Mike Bild <mike@subkit.io>
2013-11-22 09:49:57 +0100

    value and json editor done

--- files/mobile/js/app.js
+++ files/mobile/js/app.js
@@ -55,16 +55,15 @@ nav.go("center");
 .controller("StorageCtrl", ['$scope', 'angularSubkit', 'Navigation', 'shared', function StorageCtrl($scope, angularSubkit, Navigation, shared) {
 	var previous = ["stores"];
 	var nav = new Navigation();
-
-	nav.onChanged(function(name){
-		if(name === "storage") load();
-		if(name === "jsoneditor") $scope.dataObj = JSON.stringify(shared.rawObj, null, 4);
-	});
-
 	var obj = [];
 	var segments = {};
 
-	var load = function(){
+	nav.onChanged(function(name){
+		console.log(name);
+		if(name === "storage") _load();
+	});
+
+	var _load = function(){
 		var subkit = new Subkit({ baseUrl: shared.domain, apiKey: shared.apiKey });	
 		var key = previous.join('/');
 		$scope.key = key;
@@ -83,7 +82,7 @@ nav.go("center");
 		} else {
 			subkit.lookup(key, function(err, data){
 				if(err) statusCtrl.show("network error");
-				
+				console.log("load");
 				segments = {};
 				obj = data;
 				shared.rawObj = angular.isObject(data) ? data : null;
@@ -97,36 +96,17 @@ nav.go("center");
 			});
 		}
 	};
-	$scope.isJSON= function(){
-		return !angular.isArray(shared.rawObj);
-	};
-	$scope.hasParent = function(){
-		return previous.length>1;
-	};
-	$scope.json = function(){
-		nav.go("jsoneditor");
-	};
-		var c=0;
 	function _search(path, obj, segPath){
-		c++;
-		console.log(c);
 		for(var itm in obj){
 			var fullPath = [];
 			for(var p in path){
 				fullPath.push(path[p]);
 				if(path[p] === itm){
-					if(angular.isObject(obj[itm])){
-						var key = fullPath.join('/');
-						if(key === segPath){
-							return obj[itm];
-						console.log("obj");
-						console.log(key);
-						console.log(segPath);
-						}
+					if(angular.isObject(obj[itm]) && fullPath.join('/') === segPath){
+						return obj[itm];
 					} else {
 						fullPath.pop();
-						var key = fullPath.join('/');
-						if(key === segPath)
+						if(fullPath.join('/') === segPath) 
 							return obj;
 					}
 					return _search(path, obj[itm], segPath);
@@ -134,32 +114,52 @@ nav.go("center");
 			}
 		}
 	};
+
+	$scope.isJSON= function(){
+		return !angular.isArray(shared.rawObj);
+	};
+	$scope.hasParent = function(){
+		return previous.length>1;
+	};
+	$scope.json = function(){
+		$scope.jsonData = JSON.stringify(shared.rawObj, null, 4);
+		nav.go("jsoneditor");
+	};
 	$scope.edit = function(key){
+		console.log("EDIT");
 		var keys = key.split('/');
-		var prop = keys[keys.length-1];
+		var objectPropertyName = keys[keys.length-1];
 
 		var segPath = key.split('/');
 		segPath.pop();
 		segPath = segPath.join('/');
 
 		var dataSegment = _search(keys, shared.rawObj, segPath);
-		console.log(dataSegment);
-		if(dataSegment && dataSegment[prop]){
-			dataSegment[prop] = "kkk";
-		}
+		shared.objectValue = dataSegment;
+		shared.objectKey = objectPropertyName;
+
+		$scope.valueData = shared.objectValue;
+		$scope.keyData = shared.objectKey;
+
+		nav.go("valueeditor");
+	};
+	$scope.saveJson = function(){
+		console.log("save JSON");
+		console.log($scope.jsonData);
+		shared.rawObj = JSON.parse($scope.jsonData);
 		console.log(shared.rawObj);
 	};
-	$scope.save = function(){
-		console.log("save");
-		console.log($scope.dataObj);
+	$scope.saveValue = function(){
+		console.log("save Value");
+		console.log(shared.rawObj);
 	};
 	$scope.next = function(key){
 		previous.push(key);
-		load();
+		_load();
 	};
 	$scope.previous = function(){
 		previous.pop();
-		load();
+		_load();
 	};
 }]);
 

8e1d2773620d076f35b71a10b52ca8ef7ccd4baa
degir <sebastian@subkit.io>
2013-11-26 12:36:55 +0100

    5SecondNotificationBar, 20SecondNotificationBar, 1SecondNotificationBar per DI in den Controller und dann bspw. NotificationBar.PostMessage(MESSAGE, STYLEalsStringFallalsCSSvorhanden);

--- files/mobile/js/app.js
+++ files/mobile/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-.module('mobilecenter', ['app', 'subkit'])
+.module('mobilecenter', ['app', 'subkit', 'jv-NotificationBar'])
 .service('shared', ['$rootScope', function ($rootScope) {
 	var _username,
 		_password,
@@ -30,7 +30,7 @@ angular
         apiKey: _apiKey
    };
 }])
-.controller("LoginCtrl",['$scope', 'angularSubkit', 'Navigation', 'shared', function ($scope, angularSubkit, Navigation, shared) {
+.controller("LoginCtrl",['$scope', 'angularSubkit', 'Navigation', 'shared', '5SecondNotificationBar', function ($scope, angularSubkit, Navigation, shared, NotificationBar) {
 	$scope.username = "";
 	$scope.password = "";
 	$scope.domain = "";
@@ -39,9 +39,12 @@ angular
 	var nav = new Navigation();
 		
 	$scope.register = function(){
-	
+
 		//console.log($scope.newUsername);
 		//console.log($scope.newPassword);
+		
+		
+		NotificationBar.PostMessage('Registering crashed!', 'red');
 				
 		$scope.hasEnter = true;
 		var counter = $scope.loading = 5;
@@ -677,6 +680,92 @@ angular
     }
   };
 });
+
+angular.module("jv-NotificationBar", [])
+.service("NotificationBar", function($timeout, $compile, $rootScope) {
+    var domElement;
+    this.PostMessage = function(message, timeToLinger, cssToApply) {
+		console.log("notification fired!");
+        var template = angular.element("<div class=\"notification-message " + (cssToApply || "") +  "\" time=\""+timeToLinger+"\">"+message+"</div>");
+        var newScope = $rootScope.$new();
+        domElement.append($compile(template)(newScope));
+    };
+    
+    this.RegisterDOM = function(element) {
+    	domElement = element;
+    };
+    
+})
+.service("5SecondNotificationBar", function(NotificationBar) {
+    this.PostMessage = function(message, cssToApply) {
+        // This is 6000 because the CSS takes 1 second to appear 
+        // then we want it to linger for 5 seconds
+        NotificationBar.PostMessage(message, 6000, cssToApply);
+    };
+})
+.service("20SecondNotificationBar", function(NotificationBar) {
+    this.PostMessage = function(message, cssToApply) {
+        // This is 21000 because the CSS takes 1 second to appear 
+        // then we want it to linger for 20 seconds
+        NotificationBar.PostMessage(message, 21000, cssToApply);
+    };
+})
+.service("1SecondNotificationBar", function(NotificationBar) {
+    this.PostMessage = function(message, cssToApply) {
+        // This is 2000 because the CSS takes 1 second to appear, 
+        // then we want it to linger for 1 seconds
+        NotificationBar.PostMessage(message, 2000, cssToApply);
+    };
+})
+.directive("notificationBar", function(NotificationBar) {
+    return {
+        restrict:"C",
+        link: function(sc, el) {
+            NotificationBar.RegisterDOM(el);
+        }
+    }
+})
+.directive("notificationMessage", function($timeout) {
+    return {
+        restrict:"C",
+        transclude:true,
+        template: "<a href=\"javascript:void(0)\" ng-click=\"close()\">x</a><div ng-transclude></div>",
+        link: function(scope, el, attr) {
+            var promiseToEnd,
+                promiseToDestroy;
+            //ugly hack to get css styling to be interpreted correctly by browser.  Blech!
+            $timeout(function() {
+            	el.addClass("show");
+            }, 1);
+            scope.close = function() {
+                el.remove();
+                scope.$destroy();
+            };
+            
+            function cancelTimeouts() {
+                if(promiseToDestroy) {
+                    $timeout.cancel(promiseToDestroy);
+                    promiseToDestroy = undefined;
+                }
+                $timeout.cancel(promiseToEnd);
+                el.addClass("show");
+            }
+            
+            function startTimeouts() {
+            	promiseToEnd = $timeout(function() {
+                	el.removeClass("show");
+                	promiseToDestroy = $timeout(scope.close, 1010);
+            	}, attr.time);
+            }
+            
+            el.bind("mouseenter", cancelTimeouts);
+            el.bind("mouseleave", startTimeouts);
+            
+            startTimeouts();
+        }
+    };
+});
+
 
 var App = {
     init: function () {

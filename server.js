0345ead56e3c64032d602f1e424ff76b41fbb701
Mike Bild <mike@subkit.io>
2013-11-29 21:18:13 +0100

    scheduler added

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ var es = require('./lib/eventsource-module.js').init(storage, pubsub);
 var job = require('./lib/task-module.js').init(storageConfig, storage, pubsub);
 var task = require('./lib/task-module.js').init(taskConfig, storage, pubsub);
 var renderer = require("./lib/template-module.js").init({templatesPath: templateConfig.filesPath});
+var scheduler = require('./lib/jobs-module.js').init(storage);
 
 var options = { name: "SubKit" };
 
@@ -155,7 +156,7 @@ require("./lib/task.js").init(server, storage, taskConfig, task, helper);
 require("./lib/statistics.js").init(server, storage, staticConfig, helper);
 
 require("./lib/identity.js").init(server, storage, helper);
-require("./lib/email.js").init(server, emailConfig, storage, renderer, helper);
+require("./lib/email.js").init(server, emailConfig, storage, renderer, scheduler, helper);
 require("./lib/push.js").init(server, storage, helper);
 require("./lib/location.js").init(server, storage, helper);
 

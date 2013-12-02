fb5d390a8c7038dd45646c2d6bda352675b464c8
degir <sebastian@subkit.io>
2013-12-02 15:10:55 +0100

    styling

--- files/mobile/js/app.js
+++ files/mobile/js/app.js
@@ -851,6 +851,37 @@ angular
 	  ctrl.$formatters.unshift(validator);
     }
   };
+})
+.directive('uiValidateEquals', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            function validateEqual(myValue, otherValue) {
+				console.log(myValue);
+				console.log(otherValue);
+                if (myValue === otherValue) {
+                    ctrl.$setValidity('equal', true);
+                    return myValue;
+                } else {
+                    ctrl.$setValidity('equal', false);
+                    return undefined;
+                }
+            }
+
+            scope.$watch(attrs.uiValidateEquals, function(otherModelValue) {
+                validateEqual(ctrl.$viewValue, otherModelValue);               
+            });
+
+            ctrl.$parsers.unshift(function(viewValue) {
+                return validateEqual(viewValue, scope.$eval(attrs.uiValidateEquals));
+            });
+
+            ctrl.$formatters.unshift(function(modelValue) {
+                return validateEqual(modelValue, scope.$eval(attrs.uiValidateEquals));                
+            });
+        }
+    };
 });
 
 angular.module("jv-NotificationBar", [])

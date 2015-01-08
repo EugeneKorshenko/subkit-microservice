933e2d131a8c273f01ff2e5de4ffe416ad91760e
Mike Bild <mike.bild@googlemail.com>
2015-01-08 12:02:38 +0100

    Update manage.spec.js

--- test/manage.spec.js
+++ test/manage.spec.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var assert = require('assert'),
-    restify = require('restify');
+/*var assert = require('assert');
+var restify = require('restify');
 
 var client = restify.createJsonClient({
   version: '*',
   rejectUnauthorized: false,
   url: 'https://127.0.0.1:8080',
   headers: {'x-auth-token':'66LOHAiB8Zeod1bAeLYW'}
-});
+});*/
 
 describe('Integration: Manage', function(){
   var server,

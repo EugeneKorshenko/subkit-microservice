49cf2366ba4b4e7ceb054a8932321ca34be79e14
Mike Bild <mike.bild@googlemail.com>
2015-03-04 15:50:01 +0100

    Fix error response

--- routes/event.js
+++ routes/event.js
@@ -7,9 +7,8 @@ module.exports.init = function(server, event, doc){
 	(function(){
 		var count = 0;
 		setInterval(function(){
-			count++;
-			event.emit('heartbeat', {value: count});		
-		}, 1000);
+			event.emit('heartbeat', {value: count++});
+		}, 5000);
 	})();
 
 	//events
@@ -17,7 +16,7 @@ module.exports.init = function(server, event, doc){
 		var stream = req.params.stream;
 		var where = req.params.where;
 
-		if(!stream) return next(400, new Error('Parameter `stream` missing.'));
+		if(!stream) return res.send(400, new Error('Parameter `stream` missing.'));
 		
 		try { if(where) where = JSON.parse(unescape(where)); } catch(e){ where = null; }
 
@@ -32,11 +31,12 @@ module.exports.init = function(server, event, doc){
 
 	server.post('/events/bind/:stream', function(req,res,next){
 		var stream = req.params.stream;
-		var webhook = req.headers['X-Subkit-Event-WebHook'] || '';
-		var where = req.headers['X-Subkit-Event-Filter'];
+		if(!req.body) req.body = {};
+		var webhook = req.body.webhook || req.headers['x-subkit-event-webhook'];
+		var where = req.body.filter || req.headers['x-subkit-event-filter'];
 		
-		if(!stream) return next(400, new Error('Parameter `stream` missing.'));
-		if(!webhook) return next(400, new Error('Parameter `webhook` missing.'));
+		if(!stream) return res.send(400, new Error('Parameter `stream` missing.'));
+		if(!webhook) return res.send(400, new Error('Parameter `webhook` missing.'));
 		
 		try { if(where) where = JSON.parse(unescape(where)); } catch(e){ where = null; }
 
@@ -47,10 +47,10 @@ module.exports.init = function(server, event, doc){
 	});
 	server.del('/events/bind/:stream', function(req,res,next){
 		var stream = req.params.stream;
-		var webhook = req.headers['X-Subkit-Event-WebHook'];
+		var webhook = req.body.webhook || req.headers['x-subkit-event-webhook'];
 
-		if(!stream) return next(400, new Error('Parameter `stream` missing.'));
-		if(!webhook) return next(400, new Error('Parameter `webhook` missing.'));
+		if(!stream) return res.send(400, new Error('Parameter `stream` missing.'));
+		if(!webhook) return res.send(400, new Error('Parameter `webhook` missing.'));
 
 		event.unbindWebHook(stream, webhook);
 		res.send(202, {message: 'unbind accepted'});
@@ -60,13 +60,13 @@ module.exports.init = function(server, event, doc){
 	server.post('/events/emit/:stream', function (req, res, next) {
 		var stream = req.params.stream;
 		var	payload = req.body;
-		if(!stream) return next(400, new Error('Parameter `stream` missing.'));
-		if(!payload) return next(400, new Error('Parameter `payload` missing.'));
+		if(!stream) return res.send(400, new Error('Parameter `stream` missing.'));
+		if(!payload) return res.send(400, new Error('Parameter `payload` missing.'));
 
 		var isPersistent = req.headers['x-subkit-event-persistent'] || false;
 		var metadata = req.headers['x-subkit-event-metadata'] || {};
 		
-		if(!stream) return next(400, new Error('Parameter `stream` missing.'));
+		if(!stream) return res.send(400, new Error('Parameter `stream` missing.'));
 
 		event.emit(stream, payload, metadata, isPersistent, function(error, data){
 			if(error) return res.send(400, error);
@@ -79,7 +79,7 @@ module.exports.init = function(server, event, doc){
 	server.get('/events/log/:stream', function(req, res, next){
 		var stream = req.params.stream;
 		event.log(stream, {}, {}, function(err, data){
-			if(err) return next(400, err);
+			if(err) return res.send(400, err);
 			res.send(data);
 			next();
 		});
@@ -87,7 +87,7 @@ module.exports.init = function(server, event, doc){
 	server.del('/events/log/:stream', function(req, res, next){
 		var stream = req.params.stream;
 		event.deleteLog(stream, function(err){
-			if(err) return next(400, err);
+			if(err) return res.send(400, err);
 			res.send(202, {message: 'delete accepted'});
 			next();
 		});
@@ -95,7 +95,7 @@ module.exports.init = function(server, event, doc){
 
 	server.get('/events/streams', function(req,res,next){
 		event.getChannels(function(err, data){
-			if(err) return next(400, err);
+			if(err) return res.send(400, err);
 			res.send(200, data);
 			next();
 		});
@@ -105,7 +105,7 @@ module.exports.init = function(server, event, doc){
 		if(!clientId) next(400, new Error('Parameter `clientId` missing.'));
 		
 		event.getChannelsByClientId(clientId, function(err, data){
-			if(err) return next(400, err);
+			if(err) return res.send(400, err);
 			res.send(200, data);
 			next();
 		});

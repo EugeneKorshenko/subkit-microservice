949d7466ca572d9cf75cbdbc82b3cb7ee18f347c
PerhunAndrey <perhun@softproject.com.ua>
2015-04-24 15:21:39 +0300

    tests for Get available event-streams (/events/streams) and some refactoring.

--- test/integration/event.spec.js
+++ test/integration/event.spec.js
@@ -3,8 +3,16 @@
 var assert  = require('assert');
 var restify = require('restify');
 var _       = require('underscore');
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+var request = require('superagent');
 
-describe('Integration: Event', function(){
+var url = 'https://localhost:8080';
+var token = '66LOHAiB8Zeod1bAeLYW';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe.only('Integration: Event', function(){
   var server,
       context;
 
@@ -21,20 +29,146 @@ describe('Integration: Event', function(){
     setTimeout(done, 1000);
   });
 
+  describe.only('Get available event-streams:', function(){
+    it('If there is no one active stream it should return at least one stream "heartbeat"', function(done) {
+      request
+        .get(url + '/events/streams')
+        .set('X-Auth-Token', token)
+        .accept('json')
+        /*.parse( function (res, callback) {
+         res.setEncoding('utf8');
+         res.data = '';
+         res.on('data', function (chunk) {
+         res.data += chunk;
+         });
+         res.on('end', function () {
+         try {
+         callback(null, JSON.parse(res.data.toString()));
+         } catch (err) {
+         callback(err);
+         }
+         })
+         })*/
+        .end(function (res) {
+          res.status.should.be.equal(200);
+          res.body.should.be.an('array').and.have.length(1);
+          res.body.should.have.deep.property('[0].stream').and.be.equal('heartbeat');
+          done();
+        });
+    });
+    it('If there is an active stream it should return at least two streams "heartbeat" and active one', function(done) {
+      request
+        .post(url + '/events/emit/mystream')
+        .set('X-Auth-Token', token)
+        .send({Msg:'Hello Subkit!', Number: 1})
+        .accept('json')
+        .end(function (res) {
+          request
+            .get(url + '/events/streams')
+            .set('X-Auth-Token', token)
+            .accept('json')
+            .end(function (res) {
+              res.status.should.be.equal(200);
+              res.body.should.be.an('array');
+              var streams = [];
+              for (var i = 0; i < res.body.length; i++) {
+                res.body[i].should.have.property('stream');
+                streams.push(res.body[i].stream);
+              }
+              streams.should.include.members(['mystream', 'heartbeat']);
+              done();
+            });
+        });
+    });
+    it('If there is an active stream result it should contain that stream', function(done) {
+      request
+        .post(url + '/events/emit/teststream')
+        .set('X-Auth-Token', token)
+        .send({Msg:'Hello Subkit!', Number: 1})
+        .accept('json')
+        .end(function (res) {
+          request
+            .get(url + '/events/streams')
+            .set('X-Auth-Token', token)
+            .accept('json')
+            .end(function (res) {
+              res.status.should.be.equal(200);
+              res.body.should.be.an('array');
+              var streams = [];
+              for (var i = 0; i < res.body.length; i++) {
+                res.body[i].should.have.property('stream');
+                streams.push(res.body[i].stream);
+              }
+              streams.should.include.members(['teststream']);
+              done();
+            });
+        });
+    });
+    it('If there is more than one active stream result it should return all of them', function(done) {
+      request
+        .post(url + '/events/emit/teststream')
+        .set('X-Auth-Token', token)
+        .send({Msg:'Hello Subkit!', Number: 1})
+        .accept('json')
+        .end(function (res) {
+          request
+            .post(url + '/events/emit/teststream1')
+            .set('X-Auth-Token', token)
+            .send({Msg: 'Hello Subkit!', Number: 1})
+            .accept('json')
+            .end(function (res) {
+              request
+                .get(url + '/events/streams')
+                .set('X-Auth-Token', token)
+                .accept('json')
+                .end(function (res) {
+                  res.status.should.be.equal(200);
+                  res.body.should.be.an('array');
+                  var streams = [];
+                  for (var i = 0; i < res.body.length; i++) {
+                    res.body[i].should.have.property('stream');
+                    streams.push(res.body[i].stream);
+                  }
+                  streams.should.include.members(['teststream','teststream1']);
+                  done();
+                });
+            });
+        });
+    });
+  });
+
+  describe('Subscribe to specific event stream (Transfer-Encoding: chunked)', function(){});
+
+  describe('Subscribe to specific event stream (Transfer-Encoding: chunked)', function(){});
+
+  describe('Subscribe to event streams with filter (Transfer-Encoding: chunked)', function(){});
+
+  describe('Emit an event', function(){});
+
+  describe('Emit a persistent event', function(){});
+
+  describe('Read event history (stream-log)', function(){});
+
+  describe('Delete event history (stream-log)', function(){});
+
+  describe('Register a WebHook to event-stream', function(){});
+
+  describe('Unregister a WebHook from event-stream', function(){});
+
   describe('on stream', function(){
 
     var jsonClient = restify.createJsonClient({
       rejectUnauthorized: false,
-      url: 'https://127.0.0.1:8080',
-      headers: { 'x-auth-token':'66LOHAiB8Zeod1bAeLYW' }
+      url: url,
+      headers: { 'x-auth-token': token }
     });
 
     it('a emit should receive a event', function(done){
 
       var rawClient = restify.createClient({
         rejectUnauthorized: false,
-        url: 'https://127.0.0.1:8080',
-        headers: {'x-auth-token':'66LOHAiB8Zeod1bAeLYW'}
+        url: url,
+        headers: {'x-auth-token': token}
       });
 
       rawClient.get('/events/stream/mystream', function(err, req){
@@ -46,14 +180,14 @@ describe('Integration: Event', function(){
             var obj = JSON.parse(chunk.toString());
             assert.equal(obj.length, 1);
             assert.equal(obj[0].$payload.Msg, 'Hello Subkit!');
-            done();            
+            done();
           });
 
         });
 
       });
 
-      jsonClient.post('/events/emit/mystream', {Msg:'Hello Subkit!', Number: 1}); 
+      jsonClient.post('/events/emit/mystream', {Msg:'Hello Subkit!', Number: 1});
 
     });
 
@@ -63,11 +197,11 @@ describe('Integration: Event', function(){
     it('should create and call a bound webhook', function(done){
       var client = restify.createJsonClient({
         rejectUnauthorized: false,
-        url: 'https://127.0.0.1:8080',
+        url: url,
         headers: {
-          'x-auth-token':'66LOHAiB8Zeod1bAeLYW',
+          'x-auth-token': token,
           'x-subkit-event-webhook': 'https://127.0.0.1:8080/stores/events',
-          'x-subkit-event-apikey': '66LOHAiB8Zeod1bAeLYW'
+          'x-subkit-event-apikey': token
         }
       });
       client.post('/events/stream/eventstream1', {}, function(error, req, res, actual){
@@ -76,7 +210,7 @@ describe('Integration: Event', function(){
 
         client.get('/events/streams', function(error, req, res, actual){
           assert.ifError(error);
-          
+
           var inspect = _.findWhere(actual, {stream: 'eventstream1'});
           assert.notEqual(inspect, null);
           assert.equal(inspect.id, 'https://127.0.0.1:8080/stores/events');
@@ -92,7 +226,7 @@ describe('Integration: Event', function(){
               client.get('/events/streams', function(error, req, res, actual){
                 assert.ifError(error);
                 assert.equal(_.findWhere(actual, {stream: 'eventstream1'}), null);
-                done(); 
+                done();
               });
             });
           });
@@ -105,14 +239,14 @@ describe('Integration: Event', function(){
     it('should call bound webhook when filter match', function(done){
       var client = restify.createJsonClient({
         rejectUnauthorized: false,
-        url: 'https://127.0.0.1:8080',
+        url: url,
         headers: {
-          'x-auth-token':'66LOHAiB8Zeod1bAeLYW',
+          'x-auth-token': token,
           'x-subkit-event-webhook': 'https://127.0.0.1:8080/stores/events1',
-          'x-subkit-event-apikey': '66LOHAiB8Zeod1bAeLYW',
+          'x-subkit-event-apikey': token,
           'x-subkit-event-filter': JSON.stringify({content:'b'})
         }
-      });     
+      });
       client.post('/events/stream/eventstream1', {}, function(error, req, res, actual){
         assert.ifError(error);
         assert.equal(actual.message, 'created');
@@ -124,7 +258,7 @@ describe('Integration: Event', function(){
           client.del('/events/stream/eventstream1', function(error, req, res, actual){
             assert.ifError(error);
             assert.equal(actual.message, 'delete accepted');
-            done(); 
+            done();
           });
         });
       });
@@ -133,14 +267,14 @@ describe('Integration: Event', function(){
     it('should not call bound webhook when filter not match', function(done){
       var client = restify.createJsonClient({
         rejectUnauthorized: false,
-        url: 'https://127.0.0.1:8080',
+        url: url,
         headers: {
-          'x-auth-token':'66LOHAiB8Zeod1bAeLYW',
+          'x-auth-token': token,
           'x-subkit-event-webhook': 'https://127.0.0.1:8080/stores/events2',
-          'x-subkit-event-apikey': '66LOHAiB8Zeod1bAeLYW',
+          'x-subkit-event-apikey': token,
           'x-subkit-event-filter': JSON.stringify({content:'c'})
         }
-      });     
+      });
       client.post('/events/stream/eventstream1', {}, function(error, req, res, actual){
         assert.ifError(error);
         assert.equal(actual.message, 'created');
@@ -152,11 +286,11 @@ describe('Integration: Event', function(){
           client.del('/events/stream/eventstream1', function(error, req, res, actual){
             assert.ifError(error);
             assert.equal(actual.message, 'delete accepted');
-            done(); 
+            done();
           });
-          
+
         });
-      });       
+      });
     });
 
   });
